45620a2e3c4af9fcbda97fc633f359000dcf0ca5
movoid <bobrobotsun@163.com>
2024-07-04 23:46:43 +0800

    2024.07.04.01-movoid 更新1.2.11 1、确认更新

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
 
 setup(
     name='movoid_robotframework_selenium',
-    version='1.2.10.003',
+    version='1.2.11',
     packages=find_packages(),
     url='',
     license='',
