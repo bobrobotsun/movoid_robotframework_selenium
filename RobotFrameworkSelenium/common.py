9c094b32d5032b360714b8a7115f953399cc63ee
sunyifan0649.102.115 <JC0649@pmish-tech.com>
2024-08-01 16:53:45 +0800

    2024.08.01-sun.yi.fan 1、新增一个通用的teardown函数

--- RobotFrameworkSelenium/common.py
+++ RobotFrameworkSelenium/common.py
@@ -202,3 +202,10 @@ class BasicCommon(RobotBasic):
             return [locator]
         else:
             return locator
+
+    def selenium_function_debug_continue_teardown(self, function, args, kwargs, re_value, error, trace_back, has_return):
+        if error:
+            self.error(self.get_suite_case_str(), function.__name__, args, kwargs, type(error).__name__, error)
+            self.selenium_take_screenshot()
+        if has_return:
+            return re_value
