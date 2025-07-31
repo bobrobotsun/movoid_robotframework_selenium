a15aebde774730fd0065f5cd9b4f1acdd196d34f
bobrobotsun <bobrobotsun@163.com>
2025-07-31 15:48:33 +0800

    2025.07.31.1 现在截图默认不显示返回值 确认升级1.3.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
 
 setup(
     name='movoid_robotframework_selenium',
-    version='1.3.6',
+    version='1.3.7',
     packages=find_packages(),
     url='',
     license='',
